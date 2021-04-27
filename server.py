bf91fd376da3598b56f937bb112acc84f7683ad4
AnthonyLMorales <am348@buffalo.edu>
2021-04-26 20:47:14 -0400

    Button page with sockets and authentication

--- server.py
+++ server.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template, request
-import db
-app = Flask(__name__)
+import bcrypt
+from flask import Flask, render_template, request, redirect
+from flask_socketio import SocketIO
+from pymongo import MongoClient
 
+client = MongoClient("mongodb://localhost:27017/")
+
+db = client['accounts312']["users"]
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+counter = 0
 
 
 @app.route('/')
@@ -9,33 +17,62 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/sign_in',methods=['POST','GET'])
+@app.route('/sign_in', methods=['POST', 'GET'])
 def sign_in():
     if request.method == 'POST':
-        user = request.form['username']
-        passw= request.form['password']
-        if db.login(user,passw):
-            print("user signed in")
-            return render_template("index.html")
-        else:
-            return render_template("index.html")
+        username = request.form['username']
+        password = request.form['password']
+        account = list(db.find({"username": username}))
+        print(username,password)
+        if len(account) == 1:
+            acc_password = account[0].get("password")
+            print(acc_password)
+            if bcrypt.checkpw(password.encode(), acc_password):
+                return render_template("sign_in.html", success="Sign in was a success")
+
+        return render_template("sign_in.html", success="Sign in failed, try again")
+
     else:
         return render_template("sign_in.html")
 
-@app.route('/register',methods=['POST','GET'])
+
+@app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
-        user = request.form['username']
-        passw= request.form['password']
-        if db.account_exist(user):
-            return render_template("register.html")
+        username = request.form['username']
+        password = request.form['password']
+        account = list(db.find({"username": username}))
+        if len(account) == 1:
+            return render_template("register.html", success="Register Failed, try again with a different username")
         else:
-            # Todo hash password
-            db.add_account(user,passw)
-            return render_template("index.html")
+            salt = bcrypt.gensalt()
+            hashed = bcrypt.hashpw(password.encode(), salt)
+            db.insert_one({"username": username, "password": hashed})
+            print(username + " has been added")
+            print(username,password)
+            return redirect("/sign_in")
     else:
-        return render_template("register.html")
+        return render_template('register.html')
+
+
+# Handles everything for button page
+@app.route('/button')
+def button():
+    return render_template("button.html")
+
+
+@socketio.on('load_counter')
+def handle_load_count():
+    socketio.emit('receive_counter', counter)
+
+
+@socketio.on('button_click')
+def handle_button_click():
+    global counter
+    print(counter)
+    counter += 1
+    socketio.emit('receive_counter', counter)
+
 
 if __name__ == "__main__":
-    app.run()
-
+    socketio.run(app, debug=True)
