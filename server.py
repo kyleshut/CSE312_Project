f2a5336c46918978a56c16890c139413a3433784
AnthonyLMorales <am348@buffalo.edu>
2021-04-27 21:41:29 -0400

    dm room

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import bcrypt
-from flask import Flask, render_template, request, redirect
-from flask_socketio import SocketIO
+from flask import Flask, render_template, request, redirect, session
+from flask_socketio import SocketIO, emit
 from pymongo import MongoClient
 
 client = MongoClient("mongodb://localhost:27017/")
@@ -10,7 +10,9 @@ db = client['accounts312']["users"]
 app = Flask(__name__)
 socketio = SocketIO(app)
 counter = 0
+app.secret_key = "secret"
 
+dmUsers={}
 
 @app.route('/')
 def home():
@@ -19,6 +21,8 @@ def home():
 
 @app.route('/sign_in', methods=['POST', 'GET'])
 def sign_in():
+    if "user" in session:
+        return redirect("/user")
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -26,12 +30,10 @@ def sign_in():
         print(username,password)
         if len(account) == 1:
             acc_password = account[0].get("password")
-            print(acc_password)
             if bcrypt.checkpw(password.encode(), acc_password):
-                return render_template("sign_in.html", success="Sign in was a success")
-
+                session["user"] = username
+                return redirect("/user")
         return render_template("sign_in.html", success="Sign in failed, try again")
-
     else:
         return render_template("sign_in.html")
 
@@ -49,10 +51,48 @@ def register():
             hashed = bcrypt.hashpw(password.encode(), salt)
             db.insert_one({"username": username, "password": hashed})
             print(username + " has been added")
-            print(username,password)
+            print(username, password)
             return redirect("/sign_in")
     else:
         return render_template('register.html')
+
+
+@app.route('/user')
+def userpage():
+    if "user" in session:
+        user = session["user"]
+        return render_template('redirectpage.html',name=user)
+    else:
+        return redirect("/sign_in")
+
+@app.route("/logout")
+def logout():
+    session.pop("user",None)
+    return redirect("/")
+# dm room
+@app.route("/dmroom")
+def dmroom():
+    if "user" in session:
+        return render_template("dmroom.html")
+    else:
+        return redirect("/")
+
+@socketio.on('loadOnline')
+def handleConnection():
+    user = session["user"]
+    id = request.sid
+    dmUsers[user] = id
+    emit('renderOnline', dmUsers, broadcast=False)
+    emit('join',user,broadcast=True,include_self=False)
+
+
+@socketio.on('disconnect')
+def handleDis():
+    user = session["user"]
+    del dmUsers[user]
+    emit("remove_dis", user, broadcast=True,include_self=False)
+
+# end dm room
 
 
 # Handles everything for button page
@@ -72,6 +112,9 @@ def handle_button_click():
     print(counter)
     counter += 1
     socketio.emit('receive_counter', counter)
+#end buttonPage
+
+
 
 
 if __name__ == "__main__":
